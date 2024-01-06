e559f78a0ba09bcc57f9983306cf75b930b081f2
Ankit <ankit2dbg@gmail.com>
2024-01-06 21:10:38 +0530

    lastest code

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = "http://http://nextbackend.vrcwebsolutions.com"
+export const server = "http://nextbackend.vrcwebsolutions.com"
 
-export const imagePath = "http://http://nextbackend.vrcwebsolutions.com/common/"
+export const imagePath = "http://nextbackend.vrcwebsolutions.com/common/"
 
 export const handleFilterImage = (product) => {
     const image = product.img.filter(img => {
